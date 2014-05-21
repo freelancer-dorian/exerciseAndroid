9c2366f41ba435caf46e8cbca0f271c1a480f8dc
dorian <dorian.wang@outlook.com>
2014-05-21 11:29:28 +0800

    commit ui-basic

---
+++ 05-20-UIBasic/Buttons/src/com/dorian/buttons/MainActivity.java
@@ -0,0 +1,105 @@
+package com.dorian.buttons;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Button b1 = (Button) rootView.findViewById(R.id.button1);
+            
+            final Button b2 = (Button) rootView.findViewById(R.id.button2);
+            b2.setLongClickable(true);
+            b2.setClickable(false);
+            
+            final TextView tv = (TextView) rootView.findViewById(R.id.textView1);
+            
+            b2.setOnLongClickListener(new OnLongClickListener() {
+				
+				@Override
+				public boolean onLongClick(View v) {
+					Log.d("dw", "b2 long clicked!!");
+					tv.setText("b2 long clicked");
+					return false;
+				}
+			});
+            
+            b2.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Log.d("dw","b2 clicked!!");
+					tv.setText("b2 clicked");
+				}
+			});
+
+            b1.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+//					b2.setSelected(!b2.isSelected());
+					b2.setEnabled(!b2.isEnabled());
+				}
+			});
+            
+            return rootView;
+        }
+    }
+
+}
